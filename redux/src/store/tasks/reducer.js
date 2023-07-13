81f3d6c15dedc6d1c1235cc8d23afd29fa0d0fdb
Vinicius7Codeby <viniciusgabriel.pb@gmail.com>
2023-07-13 09:33:03 -0300

    feat: :art: create reducer with toolkit

--- redux/src/store/tasks/reducer.js
+++ redux/src/store/tasks/reducer.js
@@ -1,33 +1,29 @@
+import { createReducer } from '@reduxjs/toolkit'
+
 import * as actionTypes from './actionTypes'
+
+const START_STATE_VALUE = []
 
 let id = 0
 
-export default function reducer(state = [], action) {
-  switch (action.type) {
-    case actionTypes.ADD_TASK:
-      return [
-        ...state,
-        {
-          id: ++id,
-          task: action.payload.task,
-          completed: false,
-        },
-      ]
+const reducer = createReducer(START_STATE_VALUE, {
+  [actionTypes.ADD_TASK]: (state, action) => {
+    state.push({
+      id: ++id,
+      task: action.payload.task,
+      completed: false,
+    })
+  },
+  [actionTypes.REMOVE_TASK]: (state, action) => {
+    const taskIndex = state.findIndex(task => task.id === action.payload.id)
 
-    case actionTypes.REMOVE_TASK:
-      return state.filter(task => task.id !== action.payload.id)
+    state.splice(taskIndex, 1)
+  },
+  [actionTypes.TASK_COMPLETED]: (state, action) => {
+    const taskIndex = state.findIndex(task => task.id === action.payload.id)
 
-    case actionTypes.TASK_COMPLETED:
-      return state.map(
-        task => task.id === action.payload.id
-          ? {
-            ...task,
-            completed: !task.completed
-          }
-          : task
-      )
-
-    default:
-      return state
+    state[taskIndex].completed = !state[taskIndex].completed
   }
-}
+})
+
+export default reducer
--- sections-code/section5/__section5__.md
+++ sections-code/section5/__section5__.md
@@ -14,9 +14,13 @@
   const store = configureStore({ reducer })
 ```
 
-# Create Action with Redux Toolkit
+# Create ACTION with Redux Toolkit
 
 ```jsx
+  import { createAction } from '@reduxjs/toolkit'
+
+  ...
+
   const action = createAction('ACTION_NAME')
 
   const payload = { example: 'Hello World!' }
@@ -35,4 +39,40 @@
   store.dispatch(addTask({ task: 'Task Title' }))
   store.dispatch(taskCompleted({ id: 1 }))
   store.dispatch(removeTask({ id: 1 }))
+```
+
+# Create REDUCER with Redux Toolkit
+
+```jsx
+  import { createReducer } from '@reduxjs/toolkit'
+
+  ...
+
+  createReducer(START_STATE_VALUE, {
+    ACTION_NAME: (state, action) => {
+      // Modify the state here
+      // WITHOUT "return state"
+    }
+  })
+```
+
+```jsx
+  // >>> reducer.js
+  createReducer([], {
+    ADD_TASK: (state, action) => {
+      state.push({
+        id: ++id,
+        task: action.payload.task,
+        completed: false,
+      })
+    },
+    REMOVE_TASK: (state, action) => {
+      const taskIndex = state.findIndex(task => task.id === action.payload.id)
+      state.splice(taskIndex, 1)
+    },
+    TASK_COMPLETED: (state, action) => {
+      const taskIndex = state.findIndex(task => task.id === action.payload.id)
+      state[taskIndex].completed = !state[taskIndex].completed
+    }
+  })
 ```
